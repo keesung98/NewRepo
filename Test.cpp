d5c8239f5c29ed13a48d04b794cde4e285ebf4e2
KSCHOI0829-N\KSChoi0829 <kschoi>
2026-04-13 18:06:41 +0900

    [fix] 주석 Change - Year, License

--- Test.cpp
+++ Test.cpp
@@ -1,3 +1,7 @@
 // Test
 
-// This file is part of the "C++ Test" project ()
+// 주석 변경: 2018-2024년, 프로젝트 저자. 모든 권리 보유.
+
+// Copyright (c) 2018-2024, the project authors. All rights reserved.
+
+// Use of this source code is governed by a BSD-style
